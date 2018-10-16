58bb2f70c09195820b2285a18e4c19251558b52d
nabeelfarman <knabeel380@gmail.com>
2018-10-16 10:43:19 +0500

    add material file

---
+++ HaroonU/src/app/shared/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatInputModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatInputModule, 
+    MatButtonModule, 
+    MatCheckboxModule
+  ],
+  declarations: []
+})
+export class MaterialModule { }
