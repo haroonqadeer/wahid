5781682cd661eb3c5c2b6f2ae49c0d2ce0f70a44
nabeelfarman <knabeel380@gmail.com>
2018-10-17 15:12:37 +0500

    changes

--- HaroonU/src/app/app.module.ts
+++ HaroonU/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ChartModule } from 'angular-highcharts';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     MaterialModule,
     ChartModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
