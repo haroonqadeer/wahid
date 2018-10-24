0992923055e16126aecba2606b2bfe481f2b208c
nabeelfarman <knabeel380@gmail.com>
2018-10-24 11:40:59 +0500

    new project company profile

---
+++ HaroonC/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { MaterialModule } from './shared/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchPipe } from './shared/pipe-filters/pipe-search';
+import { ChartModule } from 'angular-highcharts';
+import { OrgChartModule } from 'ng2-org-chart';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CompanydashboardComponent } from './components/companydashboard/companydashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchPipe,
+    CompanydashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ChartModule,
+    OrgChartModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ HaroonC/src/app/shared/material.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule, MatPaginatorModule, MatDialogModule,  MatInputModule, MatTableModule, MatCheckboxModule, MatSelectModule, MatButtonModule, MatSidenavModule, MatCardModule, MatMenuModule, MatExpansionModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatInputModule, 
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatSidenavModule,
+    MatCardModule 
+  ],
+  exports:[    
+    MatInputModule, 
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatSidenavModule,
+    MatCardModule 
+]
+})
+export class MaterialModule { }
