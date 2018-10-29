df490bc29dda79eef732d9534caed9f717815f0a
nabeelfarman <knabeel380@gmail.com>
2018-10-29 13:17:41 +0500

    financial module

--- HaroonF/src/app/app.module.ts
+++ HaroonF/src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './shared/material.module';
+import { PNPrimeModule } from './shared/pnprime/pnprime.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchPipe } from './shared/pipe-filters/pipe-search';
+import { ChartModule } from 'angular-highcharts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ChartModule,
+    ReactiveFormsModule,
+    PNPrimeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ HaroonF/src/app/shared/material.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatNativeDateModule, MatDatepickerModule, MatFormFieldModule, MatStepperModule, MatIconModule, MatTabsModule, MatPaginatorModule, MatDialogModule,  MatInputModule, MatTableModule, MatCheckboxModule, MatSelectModule, MatButtonModule, MatSidenavModule, MatCardModule, MatMenuModule, MatExpansionModule } from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatInputModule, 
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatIconModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports:[    
+    MatInputModule, 
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatSelectModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatExpansionModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatIconModule ,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+]
+})
+export class MaterialModule { }
