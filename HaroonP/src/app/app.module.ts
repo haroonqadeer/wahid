d4bdff6a50ecfee0071dd98d517ba591455cae18
nabeelfarman <knabeel380@gmail.com>
2018-11-12 09:54:52 +0500

    new module haroonh and haroonp

---
+++ HaroonP/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './shared/material.module';
+import { PNPrimeModule } from './shared/pnprime/pnprime.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchPipe } from './shared/pipe-filters/pipe-search';
+import { ChartModule } from 'angular-highcharts';
+import {HttpModule} from '@angular/http';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SearchPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ChartModule,
+    ReactiveFormsModule,
+    PNPrimeModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
