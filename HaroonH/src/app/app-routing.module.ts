623f59ac823ebb5e10a00e592631a748acf195a4
nabeelfarman <knabeel380@gmail.com>
2018-11-14 16:12:32 +0500

    changes

--- HaroonH/src/app/app-routing.module.ts
+++ HaroonH/src/app/app-routing.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PostsComponent } from './components/posts/posts.component';
+// import { PostsComponent } from './components/posts/posts.component';
 import { RecruitmentComponent } from './components/recruitment/recruitment.component';
 import { JobprofileComponent } from './components/jobprofile/jobprofile.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: PostsComponent
+    component: RecruitmentComponent
   },
-  {
-    path: 'post',
-    component: PostsComponent
-  },
+  // {
+  //   path: 'post',
+  //   component: PostsComponent
+  // },
   {
     path: 'job',
     component: JobprofileComponent
