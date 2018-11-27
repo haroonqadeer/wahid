a3c4065bdb2e204b0ab623c658733a98585c1407
nabeelfarman <knabeel380@gmail.com>
2018-11-27 11:50:51 +0500

    changes

--- HaroonProc/src/app/app.component.ts
+++ HaroonProc/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {MenuItem} from 'primeng/api';
+import {MatBottomSheet} from '@angular/material';
+import { ErpBottomSheetComponent } from './components/erp-bottom-sheet/erp-bottom-sheet.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  
+  constructor(private bottomSheet: MatBottomSheet){
+
+  }
+
   title = 'HaroonProc';
+  
+  items: MenuItem[];
+
+  //show bottom sheet
+  showBottom(){
+    this.bottomSheet.open(ErpBottomSheetComponent);
+  }
+  
 }
