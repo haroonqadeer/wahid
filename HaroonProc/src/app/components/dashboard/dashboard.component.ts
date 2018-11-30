c3493e2a22a6d276ef3c105c1c0e73b72749ca6a
nabeelfarman <knabeel380@gmail.com>
2018-11-30 12:45:39 +0500

    changes

--- HaroonProc/src/app/components/dashboard/dashboard.component.ts
+++ HaroonProc/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'angular-highcharts';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  
+  Line_chart: Chart;
 
   constructor() { }
 
   ngOnInit() {
+    this.LineChart_init();
   }
-
+  LineChart_init() {
+    
+    let chart = new Chart({
+      chart: {
+        type: 'line'
+      },
+      title: {
+        text: '5 Years Analysis'
+      },
+      xAxis: {
+        categories: ['2011', '2012', '2013', '2014', '2015']
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        name: 'Analysis',
+        data: [4151, 5472, 3521, 14244, 7544]
+      }]
+    });
+    this.Line_chart = chart;
+  }
 }
