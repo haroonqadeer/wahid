bc6f4e54ecf484e99e4f362bfc10b4ddd4fe22eb
nabeelfarman <knabeel380@gmail.com>
2018-12-03 14:58:09 +0500

    changes

--- HaroonW/src/app/app.module.ts
+++ HaroonW/src/app/app.module.ts
@@ -1,18 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './shared/material.module';
+import { PNPrimeModule } from './shared/pnprime/pnprime.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchPipe } from './shared/pipe-filters/pipe-search';
+import { ChartModule } from 'angular-highcharts';
+import {HttpModule} from '@angular/http';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
+import { ErpBottomSheetComponent } from './components/erp-bottom-sheet/erp-bottom-sheet.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPipe,
+    ErpBottomSheetComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ChartModule,
+    ReactiveFormsModule,
+    PNPrimeModule,
+    HttpModule,
+    NgCircleProgressModule.forRoot({}),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ErpBottomSheetComponent],
 })
 export class AppModule { }
