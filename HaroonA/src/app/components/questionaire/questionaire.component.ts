bec8c1d8df2d85d8419690fc79121b7953b2e0dc
nabeelfarman <knabeel380@gmail.com>
2018-12-24 09:37:13 +0500

    changes

--- HaroonA/src/app/components/questionaire/questionaire.component.ts
+++ HaroonA/src/app/components/questionaire/questionaire.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionaireComponent implements OnInit {
 
+  public edited = false;
+  public edited1 = false;
+
+  txtPassword = '';
+  txtPin = '';
+
   constructor() { }
 
   ngOnInit() {}
