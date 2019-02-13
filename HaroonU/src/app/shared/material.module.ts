a3552d3598994790c07e348e0ace5daace223c18
AdnanAli7 <adnanali1990@gmail.com>
2019-02-13 15:47:15 +0500

    changes

--- HaroonU/src/app/shared/material.module.ts
+++ HaroonU/src/app/shared/material.module.ts
@@ -1,67 +1,42 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-<<<<<<< HEAD
-import {
-  MatBottomSheetModule,
-  MatTabsModule,
-  MatPaginatorModule,
-  MatDialogModule,
-  MatInputModule,
-  MatTableModule,
-  MatCheckboxModule,
-  MatSelectModule,
-  MatButtonModule,
-  MatSidenavModule,
-  MatCardModule,
-  MatMenuModule,
-  MatExpansionModule,
-  MatFormFieldModule
-} from '@angular/material';
-=======
-import { MatRadioModule, MatBottomSheetModule, MatTabsModule, MatPaginatorModule, MatDialogModule, MatInputModule, MatTableModule, MatCheckboxModule, MatSelectModule, MatButtonModule, MatSidenavModule, MatCardModule, MatMenuModule, MatExpansionModule } from '@angular/material';
->>>>>>> 2157057d7e66680daca3a64cd5fe7ea1cf97602e
+import { MatFormFieldModule, MatRadioModule, MatBottomSheetModule, MatTabsModule, MatPaginatorModule, MatDialogModule, MatInputModule, MatTableModule, MatCheckboxModule, MatSelectModule, MatButtonModule, MatSidenavModule, MatCardModule, MatMenuModule, MatExpansionModule } from '@angular/material';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatSelectModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatDialogModule,
-    MatTabsModule,
-    MatMenuModule,
-    MatExpansionModule,
-    MatSidenavModule,
-    MatCardModule,
-    MatBottomSheetModule,
-<<<<<<< HEAD
-    MatFormFieldModule
-=======
-    MatRadioModule
->>>>>>> 2157057d7e66680daca3a64cd5fe7ea1cf97602e
-  ],
-  exports: [
-    MatInputModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatSelectModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatDialogModule,
-    MatTabsModule,
-    MatMenuModule,
-    MatExpansionModule,
-    MatSidenavModule,
-    MatCardModule,
-    MatBottomSheetModule,
-<<<<<<< HEAD
-    MatFormFieldModule
-=======
-    MatRadioModule
->>>>>>> 2157057d7e66680daca3a64cd5fe7ea1cf97602e
-  ]
+    imports: [
+        CommonModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatSelectModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatDialogModule,
+        MatTabsModule,
+        MatMenuModule,
+        MatExpansionModule,
+        MatSidenavModule,
+        MatCardModule,
+        MatBottomSheetModule,
+        MatFormFieldModule,
+        MatRadioModule
+    ],
+    exports: [
+        MatInputModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatSelectModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatDialogModule,
+        MatTabsModule,
+        MatMenuModule,
+        MatExpansionModule,
+        MatSidenavModule,
+        MatCardModule,
+        MatBottomSheetModule,
+        MatFormFieldModule,
+        MatRadioModule
+    ]
 })
 export class MaterialModule { }
