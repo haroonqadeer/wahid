63d0795c754fb73c268cbf8893a521e5f677ad16
nabeelfarman <knabeel380@gmail.com>
2019-02-20 08:20:34 +0500

    changes

--- HaroonU/src/app/components/userroles/userroles.component.ts
+++ HaroonU/src/app/components/userroles/userroles.component.ts
@@ -1,4 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrManager } from 'ng6-toastr-notifications';
+// import { TreeNode } from '../../nodeTree/TreeNode';
+// import { NodeService } from '../../nodeTree/node.service';
+
+// import { OrderPipe } from 'ngx-order-pipe';
+
+
+declare var $: any;
+
+
+// //For Populate Data in the Employee Table
+// export interface Employee {
+//   uId: number;
+//   uName: string;
+//   uEmail: string;
+//   uRole: string;
+//   uSince: string;
+//   lastLogin: string;
+// }
+
+//For Selecting Number of Rows Display in the Table
+// export interface PageEntryValue {
+//   entryNumber: string;
+// }
 
 @Component({
   selector: 'app-userroles',
@@ -7,8 +31,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserrolesComponent implements OnInit {
 
-  public edited = false;
-  public edited1 = false;
+  order: string = "uId";
+  reverse: boolean = false;
+  //selectedEntry = "5";
+
+  // public edited = false;
+  // public edited1 = false;
 
   /// declaration
   employeeId = '';
@@ -21,32 +49,92 @@ export class UserrolesComponent implements OnInit {
   chkView = '';
   panelOpenState = true;
 
-  
-  constructor() { }
+  //Page Models
+  query = '';
+  pageEntryValue = '5';
+
+  // filesTree4: TreeNode[];
+
+  // selectedFiles2: TreeNode[];
+
+  constructor(public toastr: ToastrManager) { }
+
+  employees = [
+    {
+      uId: 1,
+      uName: 'Aamir76',
+      uEmail: 2,
+      uRole: 8
+    },
+    {
+      uId: 2,
+      uName: 'Ali456676',
+      uEmail: 4,
+      uRole: 12
+    },
+    {
+      uId: 3,
+      uName: 'Waqas445776',
+      uEmail: 3,
+      uRole: 10
+    },
+    {
+      uId: 4,
+      uName: 'Umair45676',
+      uEmail: 3,
+      uRole: 15
+    },
+    {
+      uId: 5,
+      uName: 'Touseeq5676',
+      uEmail: 6,
+      uRole: 60
+    }
+  ];
+
+  entries = [
+    { entryNumber: '5' },
+    { entryNumber: '10' },
+    { entryNumber: '15' },
+    { entryNumber: '25' },
+    { entryNumber: '50' },
+    { entryNumber: '100' }
+  ]
+
 
   ngOnInit() {
+    //this.nodeService.getFiles().then(files => this.filesTree4 = files);
+    // $(document).ready(function () {
+    //   $('#example').DataTable();
+    // });
   }
 
-  delete(){
+  // setOrder(value: string) {
+  //   if (this.order === value) {
+  //     this.reverse = !this.reverse;
+  //   }
+  //   this.order = value;
+  // }
 
-    this.edited1 = true;
-        
-    //wait 3 Seconds and hide
-    setTimeout(function() {
-        this.edited1 = false;
-        console.log(this.edited);
-    }.bind(this), 2000);
+  setOrder(value: any) {
+    if (this.order === value) {
+      this.reverse = !this.reverse;
+    }
+    this.order = value;
   }
 
-  save(){
-    
-    this.edited = true;
-        
-    //wait 3 Seconds and hide
-    setTimeout(function() {
-        this.edited = false;
-        console.log(this.edited);
-    }.bind(this), 2000);
+  delete() {
+    this.toastr.successToastr('Record Deleted Successfully', 'Error', { toastTimeout: (2500) });
+    return false;
   }
 
+  save() {
+    this.toastr.successToastr('Record Saved Successfully', 'Error', { toastTimeout: (2500) });
+    return false;
+  }
+
+  edit() {
+    this.toastr.successToastr('Record Edited Successfully', 'Error', { toastTimeout: (2500) });
+    return false;
+  }
 }
