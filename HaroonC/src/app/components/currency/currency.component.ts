ffa2161d5a531e18f62e77648b7697da567b4861
AdnanAli7 <adnanali1990@gmail.com>
2019-03-11 11:30:57 +0500

    adnan changes

--- HaroonC/src/app/components/currency/currency.component.ts
+++ HaroonC/src/app/components/currency/currency.component.ts
@@ -3,6 +3,9 @@ import { ToastrManager } from 'ng6-toastr-notifications';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { catchError, filter } from 'rxjs/operators';
+import { OrderPipe } from 'ngx-order-pipe';
+import { strictEqual } from 'assert';
+
 
 declare var $: any;
 @Component({
@@ -19,9 +22,17 @@ export class CurrencyComponent implements OnInit {
         headers: new HttpHeaders({ 'Content-Type': 'application/json' })
     }
 
-    //* variables for display values on page
+    //* variables for pagination and orderby pipe
+    p = 1;
+    order = 'info.name';
+    reverse = false;
+    sortedCollection: any[];
+    itemPerPage = '10';
 
 
+
+    //*variable for print css
+    printCss = "{table: {'border': 'solid 1px', 'width' : '100%'}}";
 
     //*Variables for NgModels
     tblSearch;
@@ -38,12 +49,38 @@ export class CurrencyComponent implements OnInit {
     currencies = [
         { currencyId: '1', currencyName: 'Rupee', countryName: 'Pakistan' },
         { currencyId: '2', currencyName: 'Doller', countryName: 'America' },
-        { currencyId: '3', currencyName: 'Pound', countryName: 'England' }
+        { currencyId: '3', currencyName: 'Pound', countryName: 'England' },
+        { currencyId: '4', currencyName: 'Euro', countryName: 'Austria' },
+        { currencyId: '5', currencyName: 'Ngultrum', countryName: 'Bhutan' },
+        { currencyId: '6', currencyName: 'Boliviano', countryName: 'Bolivia' },
+        { currencyId: '7', currencyName: 'Pula', countryName: 'Botswana' },
+        { currencyId: '8', currencyName: 'Lev', countryName: 'Bulgaria' },
+        { currencyId: '9', currencyName: 'Franc', countryName: 'Burundi' },
+        { currencyId: '10', currencyName: 'Escudo', countryName: 'Cabo Verde' },
+        { currencyId: '11', currencyName: 'Riel', countryName: 'Cambodia' },
+        { currencyId: '12', currencyName: 'Peso', countryName: 'Chile' },
+        { currencyId: '13', currencyName: 'Yuan Renminbi', countryName: 'China' },
+        { currencyId: '14', currencyName: 'Colon', countryName: 'Costa Rica' },
+        { currencyId: '15', currencyName: 'Kuna', countryName: 'Croatia' },
+        { currencyId: '16', currencyName: 'Nakfa', countryName: 'Eritrea' },
+        { currencyId: '17', currencyName: 'Birr', countryName: 'Ethiopia' },
+        { currencyId: '18', currencyName: 'Cedi', countryName: 'Ghana' },
+        { currencyId: '19', currencyName: 'Quetzal', countryName: 'Guatemala' },
+        { currencyId: '20', currencyName: 'Gourde', countryName: 'Haiti' },
+        { currencyId: '21', currencyName: 'Forint', countryName: 'Hungary' },
+        { currencyId: '22', currencyName: 'Dinar', countryName: 'Iraq' },
+        { currencyId: '23', currencyName: 'Yen', countryName: 'Japan' },
+        { currencyId: '24', currencyName: 'Tenge', countryName: 'Kazakhstan' },
+        { currencyId: '25', currencyName: 'Kip', countryName: 'Laos' }
     ];
 
-    constructor(private toastr: ToastrManager, private http: HttpClient) { }
+    constructor(private toastr: ToastrManager, private http: HttpClient, private orderPipe: OrderPipe) { }
 
     ngOnInit() {
+
+        //this.printCss = "{table : {'border': 'solid 1px'}}";
+        //alert(this.printCss);
+
     }
 
     //Function for save and update currency 
@@ -196,6 +233,67 @@ export class CurrencyComponent implements OnInit {
             this.currencies = data
         });
 
+    }
+
+    //function for sort table data 
+    setOrder(value: string) {
+
+        if (this.order === value) {
+            this.reverse = !this.reverse;
+        }
+
+        this.order = value;
+    }
+
+
+    // Function for Print Dive *******************/
+    printDiv() {
+
+        var commonCss = ".commonCss{font-family: Arial, Helvetica, sans-serif; text-align: center; }";
+
+        var cssHeading = ".cssHeading {font-size: 25px; font-weight: bold;}";
+        var cssAddress = ".cssAddress {font-size: 16px; }";
+        var cssContact = ".cssContact {font-size: 16px; }";
+
+        var tableCss = "table {width: 100%; border-collapse: collapse;}    table thead tr th {text-align: left; font-family: Arial, Helvetica, sans-serif; font-weight: bole; border-bottom: 1px solid black; margin-left: -3px;}     table tbody tr td {font-family: Arial, Helvetica, sans-serif; border-bottom: 1px solid #ccc; margin-left: -3px; height: 33px;}";
+
+        var printCss = commonCss + cssHeading + cssAddress + cssContact + tableCss;
+
+
+        //printCss = printCss + "";
+
+
+        var contents = $("#printArea").html();
+
+        var frame1 = $('<iframe />');
+        frame1[0].name = "frame1";
+        frame1.css({ "position": "absolute", "top": "-1000000px" });
+        $("body").append(frame1);
+        var frameDoc = frame1[0].contentWindow ? frame1[0].contentWindow : frame1[0].contentDocument.document ? frame1[0].contentDocument.document : frame1[0].contentDocument;
+        frameDoc.document.open();
+
+        //Create a new HTML document.
+        frameDoc.document.write('<html><head><title>DIV Contents</title>' + "<style>" + printCss + "</style>");
+
+
+        //Append the external CSS file.  <link rel="stylesheet" href="../../../styles.scss" />  <link rel="stylesheet" href="../../../../node_modules/bootstrap/dist/css/bootstrap.min.css" />
+        frameDoc.document.write('<style type="text/css" media="print">/*@page { size: landscape; }*/</style>');
+
+        frameDoc.document.write('</head><body>');
+
+        //Append the DIV contents.
+        frameDoc.document.write(contents);
+        frameDoc.document.write('</body></html>');
+        frameDoc.document.close();
+
+        //alert(frameDoc.document.head.innerHTML);
+        // alert(frameDoc.document.body.innerHTML);
+
+        setTimeout(function () {
+            window.frames["frame1"].focus();
+            window.frames["frame1"].print();
+            frame1.remove();
+        }, 500);
     }
 
 }
