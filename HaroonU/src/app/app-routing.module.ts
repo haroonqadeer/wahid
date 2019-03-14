b15f284f90d820f8020a4d3f18f2d24f81880797
nabeelfarman <knabeel380@gmail.com>
2019-03-14 09:48:37 +0500

    changes by nabeel (14-3-2019)

--- HaroonU/src/app/app-routing.module.ts
+++ HaroonU/src/app/app-routing.module.ts
@@ -29,7 +29,7 @@ const routes: Routes = [
   {
     path: 'userroles',
     component: UserrolesComponent,
-    canActivate: [AuthGuard]
+    // canActivate: [AuthGuard]
   },
   {
     path: 'userprofile',
